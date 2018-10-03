4cf70cd3df780cf7d102974a4718e22e1461cb55
ELLIOTTCABLE <me@ell.io>
2018-10-03 11:54:30 -0500

    (fix) Getting clean task working

--- src/tasks/clean.js
+++ src/tasks/clean.js
@@ -1,14 +1,15 @@
 import del from 'del';
-import path from 'path';
+import util from 'util';
 import {log} from '../utility';
 
 // default task
-export default function(gulp, config, done) {
-  let count = 0;
-  return del(config.clean).then((paths) => {
-    log('paths', paths);
-    if (count >= config.clean.lenght) {
+export default function(gulp, pkg, done) {
+  const globs = pkg.config.clean || [pkg.directories.dist, pkg.directories.tmp];
+
+  return del(globs).then((paths) => {
+    log('Paths deleted:', util.inspect(paths, {colors: true}));
+    if (paths.length >= globs.length) {
       done();
-    }
+    };
   });
 };
