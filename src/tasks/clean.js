124fcbec0fa3ffdd07d448f7d722f5b767912f46
ELLIOTTCABLE <ec@codeverse.com>
2019-01-19 17:41:29 -0600

    (new cleant) Clean ES5-lib dir by default

--- src/tasks/clean.js
+++ src/tasks/clean.js
@@ -1,15 +1,19 @@
 import del from 'del';
 import util from 'util';
-import {log} from '../utility';
+import { log } from '../utility';
 
 // default task
 export default function(gulp, pkg, done) {
-  const globs = pkg.config.clean || [pkg.directories.dist, pkg.directories.tmp];
+  const globs = pkg.config.clean || [
+    pkg.directories.dist,
+    pkg.directories.es5,
+    pkg.directories.tmp,
+  ];
 
-  return del(globs).then((paths) => {
-    log('Paths deleted:', util.inspect(paths, {colors: true}));
+  return del(globs).then(paths => {
+    log('Paths deleted:', util.inspect(paths, { colors: true }));
     if (paths.length >= globs.length) {
       done();
-    };
+    }
   });
-};
+}
